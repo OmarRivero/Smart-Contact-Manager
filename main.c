87db2e4af58cd6ff863be0730feaf2bed5a81694
Omar Rivero <imanol.rivero7@gmail.com>
2018-11-25 20:48:05 -0600

    Botones dentro del frame añadidos

--- main.c
+++ main.c
@@ -5,21 +5,35 @@
 #include "resources.h"
 #include "estructuras.h"
 
+// Identificadores de menu
 #define FILE_MENU_AGREGAR 1
 #define FILE_MENU_BUSCAR 2
 #define FILE_MENU_VER 3
 #define MENU_SALIR 4
+#define FILE_MENU_NAME 5
+
+
+// Identificadores de controles
+#define BTN_ADD_CONTACT 6
+#define BTN_FIND_CONTACT 7
+#define BTN_SEEA_CONTACT 8
 
 //La clase ventana no tiene que ver con c++
 const char g_szClassName[] = "MyWindowClass";
+
 HMENU hMenu;
+HWND hEdit; //Variable para trabajar con el text box
+
+// Prototipos de funciones
 LRESULT CALLBACK Wnd1(HWND hwnd1, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK Wnd2(HWND hwnd2, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK Wnd3(HWND hwnd3, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK Wnd4(HWND hwnd4, UINT msg, WPARAM wParam, LPARAM lParam);
 void AddMenus(HWND hwn1);
+void AddControles(HWND hwnd1);
 
 LRESULT CALLBACK Wnd1(HWND hwnd1, UINT msg, WPARAM wParam, LPARAM lParam){
+    char nuevoTitulo[200];
     switch(msg){
         case WM_COMMAND:
             switch(wParam){
@@ -32,13 +46,27 @@ LRESULT CALLBACK Wnd1(HWND hwnd1, UINT msg, WPARAM wParam, LPARAM lParam){
                 case FILE_MENU_VER:
                     MessageBeep(MB_OK);
                     break;
+                case FILE_MENU_NAME:
+                    GetWindowText(hEdit, nuevoTitulo, 100);
+                    SetWindowText(hwnd1, nuevoTitulo);
+                    break;
                 case MENU_SALIR:
                     DestroyWindow(hwnd1);
+                    break;
+                case BTN_ADD_CONTACT:
+                    MessageBeep(MB_OK);
+                    break;
+                case BTN_FIND_CONTACT:
+                    MessageBeep(MB_OK);
+                    break;
+                case BTN_SEEA_CONTACT:
+                    MessageBeep(MB_OK);
                     break;
             }
             break;
         case WM_CREATE:
             AddMenus(hwnd1);
+            AddControles(hwnd1);
             break;
         case WM_CLOSE:
             DestroyWindow(hwnd1);
@@ -83,7 +111,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         g_szClassName,
         "Smart Contact Manager",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 640, 520,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         NULL, NULL, hInstance, NULL);
 
     if(hwnd1 == NULL){
@@ -108,10 +136,18 @@ void AddMenus(HWND hwnd1){
 
     AppendMenu(hFileMenu, MF_STRING, FILE_MENU_AGREGAR, "Agregar contactos");
     AppendMenu(hFileMenu, MF_STRING, FILE_MENU_BUSCAR, "Buscar contactos");
-    AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
     AppendMenu(hFileMenu, MF_STRING, FILE_MENU_VER, "Ver todos los contactos");
-
+    //AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NAME, "Cambiar titulo de ventana"); Opcion de menu para cambiar el titulo de ventana
     AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "Opciones");
+    AppendMenu(hMenu, MF_SEPARATOR, NULL, NULL);
     AppendMenu(hMenu, MF_STRING, MENU_SALIR,"Salir");
     SetMenu(hwnd1, hMenu);
+}
+
+void  AddControles(HWND hwnd1){
+    //CreateWindowW(L"static", L"Introduce texto aqui: ", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 300, 100, 200, 50, hwnd1, NULL, NULL, NULL);
+    //hEdit = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_CHILD | WS_BORDER, 300, 170, 200, 50, hwnd1, NULL, NULL, NULL);
+    CreateWindowW(L"Button", L"Anadir contacto", WS_VISIBLE | WS_CHILD | WS_BORDER, 300, 100, 200, 50, hwnd1, BTN_ADD_CONTACT, NULL, NULL);
+    CreateWindowW(L"Button", L"Buscar contacto", WS_VISIBLE | WS_CHILD | WS_BORDER, 300, 170, 200, 50, hwnd1, BTN_FIND_CONTACT, NULL, NULL);
+    CreateWindowW(L"Button", L"Ver todos los contactos", WS_VISIBLE | WS_CHILD | WS_BORDER, 300, 240, 200, 50, hwnd1, BTN_SEEA_CONTACT, NULL, NULL);
 }
