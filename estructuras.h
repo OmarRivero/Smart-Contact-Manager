001a2bd3b5230bd197b992a9a864a5646531fc26
Omar Rivero <imanol.rivero7@gmail.com>
2018-11-25 18:02:25 -0600

    Menu simple sin acciones

--- estructuras.h
+++ estructuras.h
@@ -1,1 +1,119 @@
+/**Tabla hash abierta
+	Decidi utilizar una función hash propuesta por Brian
+	Kerninghan y Dennis Ritchie para procesar tipos de cadenas,
+	en la que basicamente se forma un numero muy grande con la
+	combinacion de los valores enteros de cada caracter y
+	finalmente se aplica un modulo para obtener un valor aceptable en
+	alguna celda de la tabla hash.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXCEL 10
 
+typedef struct moldec{
+  char *nombre;
+  struct moldec *next;
+}tcelda, *pcelda;
+
+static pcelda hashtable[MAXCEL];
+
+void makenull(void){
+	int i;
+	for(i = 0; i < MAXCEL; i++){
+		hashtable[i]->nombre = "/vacio";
+		hashtable[i] = NULL;
+	}
+}
+
+//Funcion hash
+unsigned int stringhash(char *s){
+	unsigned int h = 0;
+	for(int i = 0; *s; s++){
+		h = 131*h + *s;
+	}
+	return(h % MAXCEL);
+}
+
+pcelda search(char *s){
+	pcelda cp;
+	for(cp = hashtable[stringhash(s)]; cp != NULL; cp = cp->next){
+		if (strcmp(s, cp->nombre) == 0)
+		{
+		    printf("Ya estaba en la tabla, no se agregara\n\n");
+			return(cp); //Significa que lo encontró
+		}
+	}
+	printf("Elemento no encontrado, se agregara a la tabla...\n\n");
+}
+
+char *strsave(char *s){
+	char *p;
+	if((p = malloc(strlen(s) + 1)) != NULL)
+		strcpy(p, s);
+	return(p);
+}
+
+pcelda insertar(char *s){
+	pcelda cp;
+	int hval;
+	if((cp = search(s)) == NULL){
+		cp = (pcelda) malloc(sizeof(tcelda));
+		if(cp == NULL)
+            return NULL;
+		if((cp->nombre = strsave(s)) == NULL)
+			return NULL;
+        hval = stringhash(cp->nombre);
+		cp->next = hashtable[hval];
+		hashtable[hval] = cp;
+		display();
+		return(cp);
+	}
+}
+
+int eliminar(char *s){
+	pcelda q, cp;
+	cp = hashtable[stringhash(s)];
+	if(cp != NULL){
+		if(strcmp(s, cp->nombre) == 0){ //primero en la lista
+			hashtable[stringhash(s)] = cp->next;
+			display();
+		}
+		else
+			for(q = cp, cp = cp->next; cp != NULL; q = cp, cp->next){
+				if(strcmp(s,cp->nombre) == 0){
+					q->next = cp->next;
+					display();
+					break;
+				}
+			}
+		if (cp != NULL)
+		{
+			free((char *) cp->nombre);
+			free((pcelda) cp);
+			return 0;
+		}
+		else
+			return 1; //no lo encontró en la tabla
+	}
+	else
+		return 1; // la tabla esta vacia
+}
+
+void display(void){
+    pcelda cp;
+    printf("==================================\n");
+    printf("Tabla hash hasta el momento\n");
+	for(int i = 0; i < MAXCEL; i++){
+		cp = hashtable[i];
+		if(cp == NULL){
+            printf("/Vacio");
+		}
+		while(cp != NULL){
+            printf("%s-> ", cp->nombre);
+            cp = cp->next;
+		}
+		printf("\n");
+	}
+	printf("==================================\n\n");
+}
--- main.c
+++ main.c
@@ -5,22 +5,37 @@
 #include "resources.h"
 #include "estructuras.h"
 
+#define FILE_MEN_NEW 1
 
 //La clase ventana no tiene que ver con c++
 const char g_szClassName[] = "MyWindowClass";
+HMENU hMenu;
+LRESULT CALLBACK Wnd1(HWND hwnd1, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK Wnd2(HWND hwnd2, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK Wnd3(HWND hwnd3, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK Wnd4(HWND hwnd4, UINT msg, WPARAM wParam, LPARAM lParam);
+void AddMenus(HWND hwn1);
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+LRESULT CALLBACK Wnd1(HWND hwnd1, UINT msg, WPARAM wParam, LPARAM lParam){
     switch(msg){
+        case WM_COMMAND:
+            switch(wParam){
+                case 1:
+                    MessageBeep(MB_OK);
+                    break;
+            }
+            break;
         case WM_CREATE:
-            AddMenu();
+            AddMenus(hwnd1);
+            break;
         case WM_CLOSE:
-            DestroyWindow(hwnd);
+            DestroyWindow(hwnd1);
             break;
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
         default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
+            return DefWindowProc(hwnd1, msg, wParam, lParam);
     }
     return 0;
 }
@@ -29,13 +44,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
     WNDCLASSEX wc;
-    HWND hwnd;
+    HWND hwnd1;
     MSG msg;
 
     // Registrando la clase Window
     wc.cbSize        = sizeof(WNDCLASSEX);
     wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
+    wc.lpfnWndProc   = Wnd1;
     wc.cbClsExtra    = 0;
     wc.hInstance     = hInstance;
     wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
@@ -51,7 +66,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     }
 
     //Creacion de la ventana
-    hwnd = CreateWindowEx(
+    hwnd1 = CreateWindowEx(
         WS_EX_CLIENTEDGE,
         g_szClassName,
         "Smart Contact Manager",
@@ -59,13 +74,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         CW_USEDEFAULT, CW_USEDEFAULT, 640, 520,
         NULL, NULL, hInstance, NULL);
 
-    if(hwnd == NULL){
+    if(hwnd1 == NULL){
         MessageBox(NULL, "Fallo la creacion de la ventana!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+    ShowWindow(hwnd1, nCmdShow);
+    UpdateWindow(hwnd1);
 
     while(GetMessage(&msg, NULL, 0, 0) > 0)
     {
@@ -75,5 +90,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     return msg.wParam;
 }
 
-void AddMenu(){
+void AddMenus(HWND hwnd1){
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "Opciones");
+    SetMenu(hwnd1, hMenu);
 }
